8aa666418d87791d7f423aa14275dbb13491cf3c
randrews1372 <86131731+randrews1372@users.noreply.github.com>
2021-06-19 01:51:28 -0400

    Added locking around activity map.

--- routes.go
+++ routes.go
@@ -4,9 +4,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"log"
+	"sync"
 	"time"
 	"zgo.at/zcache"
 )
+
+// mutex is used to ensure only one request accesses the activityMap at one time. Alternative solutions are possible
+var mutex = &sync.Mutex{}
 
 // captureActivity records provided service traffic metrics.
 func captureActivity(c *fiber.Ctx) error {
@@ -22,31 +26,43 @@ func captureActivity(c *fiber.Ctx) error {
 	// Obtain activity key from path parameter. Due to routing matching, it is impossible for the activityKey to be empty
 	activityKey := c.Params("key")
 
+	// Obtain lock and use activity look-up key
+	mutex.Lock()
 	activityCache, found := activityMap[activityKey]
 
 	if !found {
+
+		// Since activity has not been previously used, create new cache instance
 		activityCache = zcache.New(time.Hour*1, time.Minute*1)
 		activityMap[activityKey] = activityCache
 	}
 
+	// Create new cache entry using provided value. Utilizing UUID to ensure each cache entry is unique
+	mutex.Unlock()
 	activityCache.Set(uuid.NewString(), activity.Value, zcache.DefaultExpiration)
 
+	// Using SendString operation rather than normal c.JSON to avoid placing word "OK" in the response body
 	c.Set("Content-Type", "application/json")
 	return c.SendString("{}")
 }
 
-// reportActivity returns service traffic metrics for the last hour.
+// reportActivity returns recent service traffic metrics.
 func reportActivity(c *fiber.Ctx) error {
 
 	// Obtain activity key from path parameter. Due to routing matching, it is impossible for the activityKey to be empty
 	activityKey := c.Params("key")
 
+	// Use struct to hold activity count since it can be returned as JSON by Go-Fiber
 	activity := activityMetric{}
 
+	// Attempt to find matching cache using provided activity key
 	activityCache, found := activityMap[activityKey]
 
 	if found {
 
+		log.Println("Found matching key entry for activityKey:", activityKey)
+
+		// Sum all activity counts present in the cache
 		for _, cacheKey := range activityCache.Keys() {
 
 			cacheEntry, isPresent := activityCache.Get(cacheKey)
@@ -55,6 +71,9 @@ func reportActivity(c *fiber.Ctx) error {
 				activity.Value += cacheEntry.(int)
 			}
 		}
+
+		log.Println("Completed summing activity counts for activityKey:", activityKey)
+
 	} else {
 		log.Println("Unable to find matching key entry for activityKey:", activityKey)
 	}
